f9b739a97428af50daf20e84281ff1b64c9f71a3
Igor Ferreira <me@igorcferreira.dev>
2022-02-03 19:17:39 -0300

    Create test to validate logic

--- src/main/kotlin/com/futureworkshops/gradle/dependency_check/model/RequestInformation.kt
+++ src/main/kotlin/com/futureworkshops/gradle/dependency_check/model/RequestInformation.kt
@@ -4,11 +4,11 @@ import java.lang.Exception
 
 data class RequestInformation(
     val module: String,
-    val version: String
+    val version: String?
 ) {
     @Throws(Exception::class)
-    fun validateAgainst(version: String) {
-        val coreInfo = ModuleInformation(version)
+    fun validateAgainst(maven: String) {
+        val coreInfo = ModuleInformation(maven)
         val exception = Exception("Please, verify if all selected plugins are compatible with core ${coreInfo.minVersion}")
 
         if (module == coreInfo.module && !coreInfo.containsVersion(version)) {
---
+++ src/test/kotlin/com/futureworkshops/gradle/dependency_check/TestCoreResolution.kt
@@ -0,0 +1,46 @@
+package com.futureworkshops.gradle.dependency_check
+
+import com.futureworkshops.gradle.dependency_check.model.RequestInformation
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import java.lang.Exception
+
+class TestCoreResolution {
+
+    private val coreMaven by lazy { "com.futureworkshops.mobileworkflow:mw-core:[2.0.0,2.1[" }
+
+    @Test
+    fun testValidVersion() {
+        val validCoreModule = "com.futureworkshops.mobileworkflow:mw-core"
+        val validCoreVersion = "2.0.0"
+
+        val request = RequestInformation(module = validCoreModule, version = validCoreVersion)
+        assertDoesNotThrow("Valid version should not throw") {
+            request.validateAgainst(coreMaven)
+        }
+    }
+
+    @Test
+    fun testVersionRange() {
+        val validCoreModule = "com.futureworkshops.mobileworkflow:mw-core"
+        val validCoreVersion = "[2.0,2.1["
+
+        val request = RequestInformation(module = validCoreModule, version = validCoreVersion)
+        assertDoesNotThrow("Valid version should not throw") {
+            request.validateAgainst(coreMaven)
+        }
+    }
+
+    @Test
+    fun testInvalidVersion() {
+        val validCoreModule = "com.futureworkshops.mobileworkflow:mw-core"
+        val validCoreVersion = "1.0.26"
+
+        val request = RequestInformation(module = validCoreModule, version = validCoreVersion)
+        assertThrows<Exception>("Invalid version should throw") {
+            request.validateAgainst(coreMaven)
+        }
+    }
+
+}
